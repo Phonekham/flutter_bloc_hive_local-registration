599883fc24187127980d47943381198f94a8ba12
Phonekham <phone.madridista@gmail.com>
2022-09-10 20:12:16 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:localregistration/services/authentication.dart';
+import 'package:localregistration/services/todo.dart';
+import 'package:localregistration/home/home.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(create: (context) => AuthenticationService()),
+        RepositoryProvider(create: (context) => TodoService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
---
+++ lib/services/authentication.dart
@@ -0,0 +1,42 @@
+import 'package:hive/hive.dart';
+import 'package:localregistration/model/user.dart';
+
+class AuthenticationService {
+  late Box<User> _users;
+
+  Future<void> init() async {
+    Hive.registerAdapter(UserAdapter());
+    _users = await Hive.openBox<User>('usersBox');
+  }
+
+  Future<String?> authenticateUser(
+      final String username, final String password) async {
+    final success = await _users.values.any((element) =>
+        element.username == username && element.password == password);
+    if (success) {
+      return username;
+    } else {
+      return null;
+    }
+  }
+
+  Future<UserCreationResult> createUser(
+      final String username, final String password) async {
+    final alreadyExists = _users.values.any(
+      (element) => element.username.toLowerCase() == username.toLowerCase(),
+    );
+
+    if (alreadyExists) {
+      return UserCreationResult.already_exists;
+    }
+
+    try {
+      _users.add(User(username, password));
+      return UserCreationResult.success;
+    } on Exception catch (ex) {
+      return UserCreationResult.failure;
+    }
+  }
+}
+
+enum UserCreationResult { success, failure, already_exists }
